9f7c524b986b9542751e9e2932912cf59e58dd4d
Saul Castañeda <scastaneda@itba.edu.ar>
2023-08-09 18:52:28 -0300

    feat: basic config for main

--- tp0/main.py
+++ tp0/main.py
@@ -1,10 +1,16 @@
+import sys
+import json
 from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect
 
 
 if __name__ == "__main__":
     factory = PokemonFactory("pokemon.json")
-    snorlax = factory.create("snorlax", 100, StatusEffect.NONE, 1)
-    print("No noise: ", attempt_catch(snorlax, "heavyball"))
+    with open(f"{sys.argv[1]}","r") as f:
+        config = json.load(f)
+        ball = config["pokeball"]
+        pokemon = factory.create(config["pokemon"], 100, StatusEffect.NONE, 1)
+    print("No noise: ", attempt_catch(pokemon, ball))
     for _ in range(10):
-        print("Noisy: ", attempt_catch(snorlax, "heavyball", 0.15))
+        print("Noisy: ", attempt_catch(pokemon, ball, 0.15))
+
