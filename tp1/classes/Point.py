11e24b2ff00ff45d30e8120d7561e10e9ab06731
lautaro <lautihernando@gmail.com>
2023-08-14 16:42:33 -0300

    feat: sokoban boilerplate

---
+++ tp1/classes/Point.py
@@ -0,0 +1,17 @@
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __str__(self):
+        return "Point(" + str(self.x) + ", " + str(self.y) + ")"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def move(self, direction):
+        return Point(self.x + direction[0], self.y + direction[1])
