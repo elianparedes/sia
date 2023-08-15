599a64db492373df06532bd3c9f0b28b61f954bd
Martin Hexht <mhecht@itba.edu.ar>
2023-08-15 17:36:02 -0300

    feat: State wrapping class "Node"

---
+++ tp1/classes/Node.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, father, state):
+        self.father = father
+        self.state = state
+
+    def getFather(self):
+        return self.father
+
+    def getState(self):
+        return self.state
+
+    def get_children(self):
+        children = []
+        for child in self.state.get_children():
+            children.append(Node(self, child))
+        return children
+
