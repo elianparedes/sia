276437f3d3cebcbc435b597f5ffed4c6fbd75ef0
Saul Castañeda <scastaneda@itba.edu.ar>
2023-08-16 16:31:39 -0300

    feat: state builder

---
+++ tp1/classes/StateBuilder.py
@@ -0,0 +1,15 @@
+class StateBuilder:
+    def __init__(self, width, height):
+        self.grid = [[' ' for _ in range(height)] for _ in range(width)]
+
+    def add_points(self, points, char):
+        for point in points:
+            self.grid[point.x][point.y] = char
+        return self
+
+    def build(self):
+        return self.grid
+
+    def print_grid(self):
+        for row in self.grid:
+            print(''.join(row))
