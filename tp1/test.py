03345177a53965df4d3b4cc6d26ddd049ddc88ae
Martin Hexht <mhecht@itba.edu.ar>
2023-08-16 16:34:09 -0300

    fix: change in test.py file

--- tp1/test.py
+++ tp1/test.py
@@ -24,13 +24,6 @@ sokoban_board = """
 """
 parsed_positions = SokobanUtils.parse_sokoban_board(sokoban_board)
 
-print("Wall positions:", parsed_positions.get('wall', []))
-print("Player position:", parsed_positions.get('player', []))
-print("Goal positions:", parsed_positions.get('goal', []))
-print("Box positions:", parsed_positions.get('box', []))
-print("Box on goal positions:", parsed_positions.get('box_on_goal', []))
-
-
 walls = parsed_positions.get('wall', [])
 blanks = parsed_positions.get('blank', [])
 boxes = parsed_positions.get('box', [])
@@ -38,9 +31,15 @@ player = parsed_positions.get('player', [])[0]
 goals = parsed_positions.get('goal', [])
 deadlocks = SokobanUtils.get_deadlocks(walls, blanks)
 
+
+print("Wall positions:", walls)
+print("Player position:", player)
+print("Goal positions:", goals)
+print("Box positions:", boxes)
+print("Box on goal positions:", parsed_positions.get('box_on_goal', []))
 print("Deadlock positions:", deadlocks)
 
-# BFS.bfs(State(parsed_positions.get('box', []), parsed_positions.get('wall', []), parsed_positions.get('player', [])[0], parsed_positions.get('goal', []), []))
-BFS.bfs(State(set(boxes), set(walls), player, set(goals), set(deadlocks)))
+#DFS.dfs(State(set(boxes), set(walls), player, set(goals), set(deadlocks)))
+#BFS.bfs(State(set(boxes), set(walls), player, set(goals), set(deadlocks)))
 #AStarSearch.a_star_search(State(set(boxes), set(walls), player, set(goals), set(deadlocks)))
 #LocalGreedy.local_greedy(State(set(boxes), set(walls), player, set(goals), set(deadlocks)))
