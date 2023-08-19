0110891f450901b41ae20a5e5542c41ae081f572
lautaro <lautihernando@gmail.com>
2023-08-19 17:25:00 -0300

    fix: fix manhattan heuristic

--- tp1/algorithms/AStar.py
+++ tp1/algorithms/AStar.py
@@ -3,7 +3,9 @@ import heapq
 from algorithms.AlgorithmABC import AlgorithmABC
 from algorithms.AlgorithmsUtils import _UtilityNode
 from classes.Node import Node
+from heuristics.HeuristicsCombination import HeuristicsCombination
 from heuristics.ManhattanDistance import ManhattanDistance
+from heuristics.MinDistance import MinDistance
 
 
 class AStar(AlgorithmABC):
@@ -18,7 +20,6 @@ class AStar(AlgorithmABC):
         while frontier:
             utility_node = heapq.heappop(frontier)
             node = utility_node.node
-
             if node.state.is_solution():
                 return node, expanded_nodes, len(frontier)
 
@@ -26,7 +27,7 @@ class AStar(AlgorithmABC):
                 new_cost = total_cost[node] + 1  # cost = 1
                 if child not in total_cost or new_cost < total_cost[child]:
                     total_cost[child] = new_cost
-                    priority = new_cost + ManhattanDistance.calculate(child.get_state())
+                    priority = new_cost + HeuristicsCombination.calculate(child.get_state(), ManhattanDistance,MinDistance)
                     heapq.heappush(frontier, _UtilityNode(child, priority))
 
             expanded_nodes += 1
--- tp1/heuristics/ManhattanDistance.py
+++ tp1/heuristics/ManhattanDistance.py
@@ -5,12 +5,17 @@ class ManhattanDistance(HeuristicABC):
     @classmethod
     def calculate(cls, state) -> int:
         total_distance = 0
+        min_for_a_box  = None
         for box in state.boxes_points:
             min_for_box = None
+            if min_for_a_box is None:
+                min_for_a_box = abs(box.x - state.player_point.x) + abs(box.y - state.player_point.y)
+            if min_for_a_box > abs(box.x - state.player_point.x) + abs(box.y - state.player_point.y):
+                min_for_a_box = abs(box.x - state.player_point.x) + abs(box.y - state.player_point.y)
             for goal in state.goals_points:
                 if min_for_box is None:
                     min_for_box = abs(box.x - goal.x) + abs(box.y - goal.y)
                 if min_for_box > abs(box.x - goal.x) + abs(box.y - goal.y):
                     min_for_box = abs(box.x - goal.x) + abs(box.y - goal.y)
             total_distance += min_for_box
-        return total_distance
+        return total_distance + min_for_a_box
