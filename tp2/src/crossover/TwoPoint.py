d12c97e064a2243d80a9485fc9cbff4c9de5ebc4
Elian Paredes <elianparedes.dev@gmail.com>
2023-09-01 21:22:46 -0300

    feat: two point crossover calculation method

---
+++ tp2/src/crossover/TwoPoint.py
@@ -0,0 +1,21 @@
+
+import random
+
+from src.crossover.CrossoverABC import CrossoverABC
+from src.classes.Gene import Gene
+
+class TwoPoint(CrossoverABC):
+    @classmethod
+    def cross(cls, gene1: Gene, gene2: Gene) -> tuple[Gene, Gene]:
+        gene1_arr = gene1.to_array()
+        gene2_arr = gene2.to_array()
+
+        last_gene_index = len(gene1_arr) - 1
+
+        first_point = random.randrange(0, last_gene_index - 1)
+        second_point = random.randrange(first_point + 1, last_gene_index)
+
+        child1 = gene1_arr[:first_point] + gene2_arr[first_point:second_point] + gene1_arr[second_point:]
+        child2 = gene2_arr[:first_point] + gene1_arr[first_point:second_point] + gene2_arr[second_point:]
+
+        return tuple([Gene.from_array(child1), Gene.from_array(child2)])
