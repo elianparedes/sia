d15f8cc148d5ff30ebf484fee4b5f4bb026ce63c
lautaro <lautihernando@gmail.com>
2023-09-04 17:25:17 -0300

    fix: change boltzmann to have class variable

--- tp2/src/selection/Boltzmann.py
+++ tp2/src/selection/Boltzmann.py
@@ -7,17 +7,41 @@ from src.selection.SelectionABC import SelectionABC
 # TODO implement generation
 # TODO get constants from config file
 class Boltzmann(SelectionABC):
+    T0 = 0
+    TC = 0
+    K = 0
+    generation = 0
+
+    @classmethod
+    def set_T0(cls, t0: float):
+        if t0 <= 0:
+            raise ValueError("Size must be greater than 0")
+        cls.T0 = t0
+    @classmethod
+    def set_TC(cls, tc: float):
+        if tc <= 0:
+            raise ValueError("Size must be greater than 0")
+        cls.TC = tc
+    @classmethod
+    def set_K(cls, k: float):
+        if k <= 0:
+            raise ValueError("Size must be greater than 0")
+        cls.K = k
+    @classmethod
+    def set_generation(cls, generation: int):
+        if generation <= 0:
+            raise ValueError("Size must be greater than 0")
+        cls.generation = generation
+
     @classmethod
     def select(cls, population: [], individuals: int):
+        if individuals <= 0 or cls.T0 <= 0 or cls.TC <= 0 or cls.K <= 0 or cls.generation <= 0:
+            raise ValueError("Individuals must be greater than 0")
         new_population = []
         fitness_values = []
         population_len = len(population)
-        generation = 10
 
-        t0 = 100
-        tc = 50
-        k = 0.1
-        temperature = tc + (t0 + tc) * math.exp(-k * generation)
+        temperature = cls.TC + (cls.T0 + cls.TC) * math.exp(-cls.K * cls.generation)
 
         total = 0
         for individual in population:
