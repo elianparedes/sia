7653316824f3154f4d9b9db5fd8695b3fc3b6f5e
Elian Paredes <elianparedes.dev@gmail.com>
2023-09-06 17:06:57 -0300

    feat: replacement methods
    Co-authored-by: Lautaro Hernando <lhernando@itba.edu.ar>
    Co-authored-by: Martín Hecht <mhecht@itba.edu.ar>

---
+++ tp2/src/replacement/Traditional.py
@@ -0,0 +1,18 @@
+from src.classes.Genotype import Genotype
+from src.replacement.ReplacementABC import ReplacementABC
+from src.selection.SelectionABC import SelectionABC
+from typing import List
+from math import ceil, floor
+
+
+class Traditional(ReplacementABC):
+    
+    @classmethod
+    def replace(cls, population: List[Genotype], children: List[Genotype], first_selection: SelectionABC , second_selection: SelectionABC, B: float = 0.5) -> List[Genotype]:
+        n = len(population)
+
+        new_population = population + children
+
+        return first_selection.select(new_population, ceil(n * B)) + second_selection.select(new_population, floor(n * (1 - B)))
+
+        
