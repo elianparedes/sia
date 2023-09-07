3ca5f83e2bdd648adcc8ad46001be71253aa1c12
Juan Burda <jburda@itba.edu.ar>
2023-09-06 22:27:01 -0300

    fix: young bias

--- tp2/src/replacement/Traditional.py
+++ tp2/src/replacement/Traditional.py
@@ -6,13 +6,13 @@ from math import ceil, floor
 
 
 class Traditional(ReplacementABC):
-    
+
     @classmethod
-    def replace(cls, population: List[Genotype], children: List[Genotype], first_selection: SelectionABC , second_selection: SelectionABC, B: float = 0.5) -> List[Genotype]:
+    def replace(cls, population: List[Genotype], children: List[Genotype], first_selection: SelectionABC,
+                second_selection: SelectionABC, B: float = 0.5) -> List[Genotype]:
         n = len(population)
 
         new_population = population + children
 
-        return first_selection.select(new_population, ceil(n * B)) + second_selection.select(new_population, floor(n * (1 - B)))
-
-        
+        return first_selection.select(new_population, ceil(n * B)) + second_selection.select(new_population,
+                                                                                             floor(n * (1 - B)))
--- tp2/src/replacement/YoungBias.py
+++ tp2/src/replacement/YoungBias.py
@@ -4,17 +4,18 @@ from src.selection.SelectionABC import SelectionABC
 from typing import List
 from math import ceil, floor
 
+
 class YoungBias(ReplacementABC):
-    
+
     @classmethod
-    def replace(cls, population: List[Genotype], children: List[Genotype], first_selection: SelectionABC , second_selection: SelectionABC, B: float = 0.5) -> List[Genotype]:
+    def replace(cls, population: List[Genotype], children: List[Genotype], first_selection: SelectionABC,
+                second_selection: SelectionABC, B: float = 0.5) -> List[Genotype]:
         n = len(population)
         k = len(children)
 
-        if (k > n):
-            return first_selection(children, ceil(n * B)) + second_selection(children, floor(n * (1 - B)))
+        if k > n:
+            return first_selection.select(children, ceil(n * B)) + second_selection.select(children, floor(n * (1 - B)))
 
         else:
-            return children + first_selection(population, ceil((n - k) * B)) + second_selection(population, floor((n - k) * (1 - B)))
-
-        
+            return children + first_selection.select(population, ceil((n - k) * B)) + second_selection.select(
+                population, floor((n - k) * (1 - B)))
