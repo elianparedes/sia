6f333e3d11d6d62886d0dbb637a19ad66e6037de
lautaro <lautihernando@gmail.com>
2023-09-16 19:54:26 -0300

    feat: add plot to ej 1

--- tp3/src/functions/linear_perceptor.py
+++ tp3/src/functions/linear_perceptor.py
@@ -1,6 +1,8 @@
 import random
 import sys
 import numpy as np
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 values = [[-1, 1], [1, -1], [-1, -1], [1, 1]]
 expected = [-1, -1, -1, 1]
@@ -57,9 +59,49 @@ def initialize_weights():
     return w
 
 
-print(linear_perceptor())
+print(linear_perceptor())  # Crear la figura de Plotly con subplots
 
+# Crear la figura de Plotly con subplots
+fig = make_subplots(rows=1, cols=1)
 
+# Definir el rango de valores de x
+x_values = np.linspace(-10, 10, 100)  # Puedes ajustar el rango según tus necesidades
 
+# Inicializar una lista para almacenar los datos de cada trazo
+traces = []
 
+colors = []
+for i in range(len(ws)):
+    colors.append('#%06X' % random.randint(0, 0xFFFFFF))
 
+# Configurar los fotogramas de la animación
+animation_frames = []
+
+for i, w in enumerate(ws):
+    y_values = (-w[0] - w[1] * x_values) / w[2]
+    trace = go.Scatter(x=x_values, y=y_values, mode='lines', name=f'Recta {i + 1}', line=dict(color=colors[i]))
+    traces.append(trace)
+
+# Crear los fotogramas de animación
+for i, trace in enumerate(traces):
+    frame = go.Frame(data=trace)
+    animation_frames.append(frame)
+# Agregar los fotogramas de animación a la figura
+
+fig = go.Figure(
+    data=[traces[0],go.Scatter(x= [val[0] for val in values], y= [val[1] for val in values],mode = 'markers')],
+    layout=go.Layout(
+        xaxis=dict(range=[-10, 10], autorange=False),
+        yaxis=dict(range=[-10, 10], autorange=False),
+        title="Start Title",
+        updatemenus=[dict(
+            type="buttons",
+            buttons=[dict(label="Play",
+                          method="animate",
+                          args=[None])])]
+    ),
+    frames=animation_frames,
+
+)
+
+fig.show()
