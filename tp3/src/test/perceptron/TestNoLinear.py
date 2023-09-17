d4840d4dd62173a315a40d877dcaea0fea0f4c02
lautaro <lautihernando@gmail.com>
2023-09-17 18:14:03 -0300

    fix: change initialize weights to no linear test

--- tp3/src/test/perceptron/TestNoLinear.py
+++ tp3/src/test/perceptron/TestNoLinear.py
@@ -1,5 +1,5 @@
+import random
 import sys
-from random import random
 
 import numpy as np
 import os as os
@@ -13,7 +13,7 @@ ws = []
 
 def initialize_weights():
     w = []
-    for i in range(0, 2):
+    for i in range(0, 3):
         w.append(random.uniform(-1, 1))
     return w
 
@@ -32,9 +32,9 @@ def create_dataframe():
 def no_linear_perceptron():
     inputs, expected = create_dataframe()
     i = 0
-    limit = 1000000
+    limit = 100000
     w = initialize_weights()
-    perceptron = NoLinear(inputs, expected, w, learning_rate, (lambda x, beta: 1 / (1 + np.exp(-2*beta * x))), (lambda x, beta: 0.5* beta * np.log(1+np.exp(-2*beta*x))), 1)
+    perceptron = NoLinear(inputs, expected, w, learning_rate, (lambda x, beta: 1 / (1 + np.exp(-2*beta * x))), (lambda x, beta: 0.5* beta * np.log(1+np.exp(-2*beta*x))), 0.000001)
     ws.append(w)
     min_error = sys.maxsize
     w_min = None
