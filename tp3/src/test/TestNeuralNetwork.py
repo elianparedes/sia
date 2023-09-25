dd120c1f724dfb5cea5e69dd9f28f31b78d7e75c
Juan Burda <jburda@itba.edu.ar>
2023-09-24 22:10:59 -0300

    feat: added network even and digits test

--- tp3/src/test/TestNeuralNetwork.py
+++ tp3/src/test/TestNeuralNetwork.py
@@ -3,46 +3,74 @@ import random
 import sys
 import time
 
-import numpy as np
 from src.classes.NeuralNetwork import NeuralNetwork
 from src.utils import Function
+from src.utils.ExerciseUtils import ExerciseUtils
 
-DIGITS_PATH = os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, os.path.pardir,
+DIGITS_PATH = os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir,
                            "Data", "TP3-ej3-digitos.txt")
 
-values = [[0, 1], [1, 0], [0, 0], [1, 1]]
-expected = [[1], [1], [0], [0]]
 
-
-def neural_network_test():
-    inputs = values
-    network = NeuralNetwork([2, 3, 1], Function.TAN_H, Function.TAN_H_DERIVATIVE, Function.TAN_H,
+def neural_network_test(dataset, expected, architecture):
+    network = NeuralNetwork(architecture, Function.TAN_H, Function.TAN_H_DERIVATIVE, Function.TAN_H,
                             Function.TAN_H_DERIVATIVE, 0.1, [-1, 1])
     i = 0
     min_error = sys.maxsize
     limit = 5000
-    epsilon = 0.01
+    epsilon = 0.001
     w_min = None
     start = time.process_time()
+
     while min_error > epsilon and i < limit:
-        mu = random.randint(0, len(inputs) - 1)
-        network.forward_propagation(inputs[mu])
+        mu = random.randint(0, len(dataset) - 1)
+        network.forward_propagation(dataset[mu])
         network.back_propagation(expected[mu])
         w = network.set_delta_w()
-        error = network.compute_error(inputs, expected)
+        error = network.compute_error(dataset, expected)
         if error < min_error:
             min_error = error
             w_min = w
         i += 1
+
     end = time.process_time()
-    print(network.test_forward_propagation_custom([0, 0], w_min))
-    print(network.test_forward_propagation_custom([0, 1], w_min))
-    print(network.test_forward_propagation_custom([1, 0], w_min))
-    print(network.test_forward_propagation_custom([1, 1], w_min))
-    print(min_error)
-    print(end - start)
-    print(i)
+    for k in range(0, len(dataset)):
+        print('expected: ', expected[k], '-> result: ', network.test_forward_propagation_custom(dataset[k], w_min)
+              .round(3))
+
+    print('min_error: ', min_error)
+    print('time: ', end - start)
+    print('iterations: ', i)
     return w_min
 
 
-neural_network_test()
+def test_xor():
+    dataset = [[0, 1], [1, 0], [0, 0], [1, 1]]
+    expected = [[1], [1], [0], [0]]
+    architecture = [2, 3, 1]
+    neural_network_test(dataset, expected, architecture)
+
+
+def test_even():
+    dataset = ExerciseUtils.load_ex3_file(DIGITS_PATH)
+    expected = [[1], [0], [1], [0], [1], [0], [1], [0], [1], [0]]
+    architecture = [35, 1]
+    neural_network_test(dataset, expected, architecture)
+
+
+def test_digits():
+    dataset = ExerciseUtils.load_ex3_file(DIGITS_PATH)
+    expected = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
+    architecture = [35, 10]
+    neural_network_test(dataset, expected, architecture)
+
+
+test_digits()
