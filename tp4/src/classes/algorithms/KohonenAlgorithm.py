06e10935cec68e78e74ea7cbc4180009c10b4e0b
Elian Paredes <elianparedes.dev@gmail.com>
2023-10-12 12:08:29 -0300

    fix: learning rate not being decremented on iteration
    Co-authored-by: Nicolás Margenat <margenatnico@gmail.com>
    Co-authored-by: Lautaro Hernando <lhernando@itba.edu.ar>
    Co-authored-by: Martín Hecht <mhecht@itba.edu.ar>

--- tp4/src/classes/algorithms/KohonenAlgorithm.py
+++ tp4/src/classes/algorithms/KohonenAlgorithm.py
@@ -2,11 +2,15 @@ class KohonenAlgorithm:
     @staticmethod
     def train(network, training_set, learning_rate, radius):
         for i in range(1000 * network.neuron_qty):
+            learning_rate = 1 / i if i != 0 else 1
+
+            radius = (1000 * network.neuron_qty - i) * radius / (1000 * network.neuron_qty)
+            if radius < 1:
+                radius = 1
+                
             for register in training_set:
                 winner_neuron, _ = network.get_winner_neuron(register)
-                # radius = (2000 * network.neuron_qty - i) * radius / (2000 * network.neuron_qty)
-                # if radius < 1:
-                #     radius = 1
+               
                 neighbours = network.get_neighbours(winner_neuron.x, winner_neuron.y, radius)
                 for neighbour in neighbours:
                     neighbour.set_weights(network.kohonen_rule(neighbour, register, learning_rate))
