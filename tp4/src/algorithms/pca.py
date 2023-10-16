9008a076b0615e757ccb73c6f83d065ca34c196e
Martin Hexht <mhecht@itba.edu.ar>
2023-10-16 02:07:19 -0300

    Feat: homemade and scikit pca

--- tp4/src/algorithms/pca.py
+++ tp4/src/algorithms/pca.py
@@ -8,6 +8,10 @@ from src.classes.algorithms.PCAAlgorithm import PCAAlgorithm
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "config.json")
 with open(CONFIG_PATH, 'r') as f:
@@ -32,5 +36,46 @@ with open(INPUT_PATH, 'r') as file:
         INPUT.append(row[1:])
         COUNTRIES.append(row[0])
 
-pca_values = PCAAlgorithm.train(INPUT)
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(INPUT)
+
+
+# Implementacion casera
+pc1, pc2 = PCAAlgorithm.train(scaled_data)
+
+pca_df = pd.DataFrame({"PC1": pc1, "PC2": pc2, "Country": COUNTRIES})
+plt.figure(figsize=(10, 8))
+plt.scatter(pca_df["PC1"], pca_df["PC2"])
+plt.xlabel("Principal Component 1 (PC1)")
+plt.ylabel("Principal Component 2 (PC2)")
+plt.title("PCA Analysis")
+plt.grid()
+for i, country in enumerate(pca_df["Country"]):
+    plt.annotate(country, (pca_df["PC1"][i], pca_df["PC2"][i]))
+plt.show()
+
+
+# Implementacion de scikit
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(scaled_data)
+
+pca_df = pd.DataFrame(data=pca_result, columns=["PC1", "PC2"])
+pca_df["Country"] = COUNTRIES
+plt.figure(figsize=(10, 8))
+plt.scatter(pca_df["PC1"], pca_df["PC2"])
+plt.xlabel("Principal Component 1 (PC1)")
+plt.ylabel("Principal Component 2 (PC2)")
+plt.title("PCA Analysis")
+plt.grid()
+for i, country in enumerate(pca_df["Country"]):
+    plt.annotate(country, (pca_df["PC1"][i], pca_df["PC2"][i]))
+plt.show()
+
+
+
+
+
+
+
+
 
--- tp4/src/classes/algorithms/PCAAlgorithm.py
+++ tp4/src/classes/algorithms/PCAAlgorithm.py
@@ -6,14 +6,21 @@ import scipy.stats as stats
 class PCAAlgorithm:
     @staticmethod
     def train(input):
+
         input = pd.DataFrame(input)
         correlation_matrix = input.corr()
+
+        correlation_matrix = np.array(correlation_matrix)
+        input = np.array(input)
+
         eigenvalues, eigenvectors = np.linalg.eig(correlation_matrix)
-        biggest_eigenvalue_index = np.argmax(eigenvalues)
-        biggest_eigenvector = eigenvectors[biggest_eigenvalue_index]
-        output_vector = []
+
+        pc1 = []
+        pc2 = []
         for row in input:
-            y_value = np.dot(biggest_eigenvector, row)
-            output_vector.append(y_value)
-        return output_vector
+            y_value1 = np.dot(eigenvectors.T[0], row)
+            y_value2 = np.dot(eigenvectors.T[1], row)
+            pc1.append(y_value1)
+            pc2.append(y_value2)
+        return pc1, pc2
 
