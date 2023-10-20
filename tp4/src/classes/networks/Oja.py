530ba551e152b90b08818b440f41a47a3fddd36d
lautaro <lautihernando@gmail.com>
2023-10-20 19:06:21 -0300

    feat: create Oja network

---
+++ tp4/src/classes/networks/Oja.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from src.classes.neurons.SimpleNeuron import SimpleNeuron
+
+
+class Oja:
+
+    def __init__(self,neuron_quantity, weight_calculator):
+        self.neuron_quantity = neuron_quantity
+        self.neurons = SimpleNeuron(neuron_quantity, weight_calculator)
+    def get_activation(self, input_vector):
+        weights = self.neurons.get_weights()
+        return np.dot(input_vector, weights)
+    def update_weights(self, activation, learning_rate, input_vector):
+        weights = self.neurons.get_weights()
+        delta_weights = learning_rate * (input_vector * activation - (activation ** 2) * weights)
+        self.neurons.set_weights(weights + delta_weights)
