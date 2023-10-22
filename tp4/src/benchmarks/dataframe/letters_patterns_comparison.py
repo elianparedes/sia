4d8875417f098b42941b8c8f7d1978d36d1a17ea
Juan Burda <jburda@itba.edu.ar>
2023-10-22 16:13:07 -0300

    refactor: LettersUtils

--- tp4/src/benchmarks/dataframe/letters_patterns_comparison.py
+++ tp4/src/benchmarks/dataframe/letters_patterns_comparison.py
@@ -1,40 +1,19 @@
-import ast
 import os
 import itertools
 import numpy as np
 import pandas as pd
-from string import ascii_lowercase as alc
+
+from src.utils.LettersUtils import LettersUtils
 
 FILE_PATH = os.path.join(os.pardir, os.pardir, os.pardir, "data", "letters.csv")
-
-
-def load_letters_map_from_file(file_path):
-    letters = []
-
-    with open(file_path, 'r') as f:
-        for line in f:
-            line = line.strip()
-            matrix = ast.literal_eval(line)  # Convert string representation of list to actual list
-            letters.append(matrix)
-
-    alphabet = []
-    for i in alc:
-        alphabet.append(i)
-
-    letters_map = {}
-    for i, letter in enumerate(letters):
-        letters_map[alphabet[i]] = letter
-
-    return letters_map
 
 
 def letters_patterns_comparison_df():
 
     avg_dot_product = []
     max_dot_product = []
-    rows = []
 
-    letters = load_letters_map_from_file(FILE_PATH)
+    letters = LettersUtils.load_letters_map_from_file(FILE_PATH)
     combinations = itertools.combinations(letters.keys(), 4)
     for i, combination in enumerate(combinations):
         letters_combination = []
@@ -53,4 +32,4 @@ def letters_patterns_comparison_df():
     df = pd.DataFrame(sorted(avg_dot_product), columns=["|<,>| medio", "grupo"])
     df2 = pd.DataFrame(sorted(max_dot_product), columns=["|<,>| max", "grupo"])
 
-letters_patterns_comparison_df()
+    return df, df2
