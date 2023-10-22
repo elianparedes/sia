9692642d0ae17c36381bca0d1b43d2ea1e236b34
Nicolás Margenat <nmargenat@itba.edu.ar>
2023-10-21 21:23:14 -0300

    feat: add plots for umatrix

--- tp4/src/classes/networks/Oja.py
+++ tp4/src/classes/networks/Oja.py
@@ -1,16 +1,19 @@
 import numpy as np
 
+from src.classes.networks.NetworkABC import NetworkABC
 from src.classes.neurons.SimpleNeuron import SimpleNeuron
 
 
-class Oja:
+class Oja(NetworkABC):
 
-    def __init__(self,neuron_quantity, weight_calculator):
+    def __init__(self, neuron_quantity, weight_calculator):
         self.neuron_quantity = neuron_quantity
         self.neurons = SimpleNeuron(neuron_quantity, weight_calculator)
+
     def get_activation(self, input_vector):
         weights = self.neurons.get_weights()
         return np.dot(input_vector, weights)
+
     def update_weights(self, activation, learning_rate, input_vector):
         weights = self.neurons.get_weights()
         delta_weights = learning_rate * (input_vector * activation - (activation ** 2) * weights)
