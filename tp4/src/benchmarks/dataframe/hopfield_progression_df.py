6d5bee485d8f5bb4933c98032891451af70e0d25
Juan Burda <jburda@itba.edu.ar>
2023-10-23 16:27:21 -0300

    feat: stored patterns in plot

--- tp4/src/benchmarks/dataframe/hopfield_progression_df.py
+++ tp4/src/benchmarks/dataframe/hopfield_progression_df.py
@@ -3,27 +3,38 @@ from src.classes.networks.Hopfield import Hopfield
 
 def hopfield_progression_df(epochs, patterns, input_state, grid_size):
     network = Hopfield(patterns, input_state)
-    hopfield_arrays = []
-    hopfield_array = [[] for _ in range(grid_size)]
+    heatmap_patterns = []
+    __convert_patterns_to_heatmap(network, heatmap_patterns, grid_size)
+    heatmaps_by_epoch = []
+    __convert_states_to_heatmap(network, heatmaps_by_epoch, grid_size)
+    for i in range(0, epochs):
+        network.train_epoch()
+        __convert_states_to_heatmap(network, heatmaps_by_epoch, grid_size)
+    return heatmaps_by_epoch, heatmap_patterns
+
+
+def __convert_states_to_heatmap(network, heatmaps, grid_size):
+    heatmap = [[] for _ in range(grid_size)]
     i = 0
     j = 0
     for state in network.get_states():
-        hopfield_array[i].append(state)
+        heatmap[i].append(state)
         j += 1
         if j % grid_size == 0:
             i += 1
             j = 0
-    hopfield_arrays.append(hopfield_array)
-    for i in range(0, epochs):
-        network.train_epoch()
-        hopfield_array = [[] for _ in range(grid_size)]
+    heatmaps.append(heatmap)
+
+
+def __convert_patterns_to_heatmap(network, heatmaps, grid_size):
+    for pattern in network.patterns:
+        heatmap = [[] for _ in range(grid_size)]
         i = 0
         j = 0
-        for state in network.get_states():
-            hopfield_array[i].append(state)
+        for value in pattern:
+            heatmap[i].append(value)
             j += 1
             if j % grid_size == 0:
                 i += 1
                 j = 0
-        hopfield_arrays.append(hopfield_array)
-    return hopfield_arrays
+        heatmaps.append(heatmap)
--- tp4/src/benchmarks/plot/hopfield_progression_plot.py
+++ tp4/src/benchmarks/plot/hopfield_progression_plot.py
@@ -15,7 +15,7 @@ def hopfield_progression_plot(patterns, input_pattern, epochs, grid_size):
     fig.update_xaxes(scaleanchor="y", scaleratio=1)
     fig.update_yaxes(scaleanchor="x", scaleratio=1)
 
-    hopfield_arrays = hopfield_progression_df(epochs, patterns, input_pattern, grid_size)
+    heatmaps_by_epoch, heatmaps_pattern = hopfield_progression_df(epochs, patterns, input_pattern, grid_size)
 
     custom_colorscale = [
         [0, 'white'],
@@ -24,13 +24,33 @@ def hopfield_progression_plot(patterns, input_pattern, epochs, grid_size):
     ]
 
     for i in range(0, epochs):
-        data = hopfield_arrays[i][::-1]
+        data = heatmaps_by_epoch[i][::-1]
         heatmap = go.Heatmap(z=data, colorscale=custom_colorscale)
         fig.add_trace(heatmap, row=1, col=i + 1)
         fig.update_xaxes(scaleanchor="y", scaleratio=1)
         fig.update_yaxes(scaleanchor="x", scaleratio=1)
 
     fig.update_layout(title="Neuron states by epochs")
+    fig.show()
+
+    # Patterns plot
+
+    fig = make_subplots(rows=1, cols=len(patterns))
+
+    custom_colorscale = [
+        [0, 'white'],
+        [0.5, 'blue'],
+        [1, 'darkblue']
+    ]
+
+    for i in range(0, len(patterns)):
+        data = heatmaps_pattern[i][::-1]
+        heatmap = go.Heatmap(z=data, colorscale=custom_colorscale)
+        fig.add_trace(heatmap, row=1, col=i + 1)
+        fig.update_xaxes(scaleanchor="y", scaleratio=1)
+        fig.update_yaxes(scaleanchor="x", scaleratio=1)
+
+    fig.update_layout(title="Stored patterns in Hopfield network")
     fig.show()
 
 
@@ -56,5 +76,5 @@ def big_letter_progression_plot(selected_letters, input_letter, epochs):
     hopfield_progression_plot(patterns, input_pattern, epochs, 13)
 
 
-letter_progression_plot(['a', 'b', 'c', 'd'], 'z', 10)
-big_letter_progression_plot(['a', 'b', 'c', 'd'], 'a', 10)
+letter_progression_plot(['a', 'b', 'c', 'd'], 'z', 5)
+big_letter_progression_plot(['a', 'b', 'c', 'd'], 'a', 5)
