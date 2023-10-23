bb420a50c0d84a9a0c3a26897abe7524d158bcb1
Juan Burda <jburda@itba.edu.ar>
2023-10-23 15:44:28 -0300

    refactor: hopfield plots

--- tp4/src/benchmarks/dataframe/hopfield_progression_df.py
+++ tp4/src/benchmarks/dataframe/hopfield_progression_df.py
@@ -1,29 +1,29 @@
 from src.classes.networks.Hopfield import Hopfield
 
 
-def letters_progression_df(epochs, patterns, input_state, grid_size):
+def hopfield_progression_df(epochs, patterns, input_state, grid_size):
     network = Hopfield(patterns, input_state)
-    letters_arrays = []
-    letter_arrays = [[] for _ in range(grid_size)]
+    hopfield_arrays = []
+    hopfield_array = [[] for _ in range(grid_size)]
     i = 0
     j = 0
     for state in network.get_states():
-        letter_arrays[i].append(state)
+        hopfield_array[i].append(state)
         j += 1
         if j % grid_size == 0:
             i += 1
             j = 0
-    letters_arrays.append(letter_arrays)
+    hopfield_arrays.append(hopfield_array)
     for i in range(0, epochs):
         network.train_epoch()
-        letter_arrays = [[] for _ in range(grid_size)]
+        hopfield_array = [[] for _ in range(grid_size)]
         i = 0
         j = 0
         for state in network.get_states():
-            letter_arrays[i].append(state)
+            hopfield_array[i].append(state)
             j += 1
             if j % grid_size == 0:
                 i += 1
                 j = 0
-        letters_arrays.append(letter_arrays)
-    return letters_arrays
+        hopfield_arrays.append(hopfield_array)
+    return hopfield_arrays
