fix: adam optimizer result sign
Co-authored-by: Lautaro Hernando <lhernando@itba.edu.ar>

tp5_/classes/layer/Layer.py
@@ -34,7 +34,7 @@ class Layer:
         weights_error = np.dot(self.layer_input.T, output_error)  # delta w
 
         # update parameters
-        self.weights -= self.optimizer.calculate_delta_w(weights_error, self.layer_input, epoch)
+        self.weights += self.optimizer.calculate_delta_w(weights_error, self.layer_input, epoch)
 
         return input_error
     

tp5_/classes/optimizers/Adam.py
@@ -25,4 +25,4 @@ class Adam():
         m = np.divide(self.m, 1 - self.b1 ** (epoch + 1))
         v = np.divide(self.v, 1 - self.b2 ** (epoch + 1))
 
-        return self.learning_rate * np.divide(m, (np.sqrt(v) + self.eps))
+        return -self.learning_rate * np.divide(m, (np.sqrt(v) + self.eps))