99b62fa58b811f347b40dd731a00c997c99c390b
Elian Paredes <elianparedes.dev@gmail.com>
2023-11-20 19:38:09 -0300

    fix: denoising autoencoder not recovering numbers properly
    Co-authored-by: Lautaro Hernando <lhernando@itba.edu.ar>
    Co-authored-by: Juan Burda <jburda@itba.edu.ar>

--- tp5_/benchmarks/denoising.py
+++ tp5_/benchmarks/denoising.py
@@ -11,27 +11,24 @@ from classes.utils.TrainingUtils import TrainingUtils
 from data import get_characters
 
 # Change params
-EPOCHS = 1000000
-LEARNING_RATE = 0.001
+EPOCHS = 50000
+LEARNING_RATE = 0.0001
 MEAN = 0
-STD_DEVIATION = 0.1
+STD_DEVIATION = 0.09
 
 # ADAM = 0.0001 works better
 
 # Setup nn
 net = NeuralNetwork()
 
-net.add(Layer(35, 64, TAN_H, TAN_H_DERIVATIVE, Adam()))
-net.add(Layer(64, 64, TAN_H, TAN_H_DERIVATIVE, Adam()))
-net.add(Layer(64, 64, TAN_H, TAN_H_DERIVATIVE, Adam()))
-net.add(Layer(64, 64, TAN_H, TAN_H_DERIVATIVE, Adam()))
-net.add(Layer(64, 2, TAN_H, TAN_H_DERIVATIVE, Adam()))
-net.add(Layer(2, 64, TAN_H, TAN_H_DERIVATIVE, Adam()))
-net.add(Layer(64, 64, TAN_H, TAN_H_DERIVATIVE, Adam()))
-net.add(Layer(64, 64, TAN_H, TAN_H_DERIVATIVE, Adam()))
-net.add(Layer(64, 64, TAN_H, TAN_H_DERIVATIVE, Adam()))
-net.add(Layer(64, 35, TAN_H, TAN_H_DERIVATIVE, Adam()))
-
+net.add(Layer(35, 25, TAN_H, TAN_H_DERIVATIVE, Adam()))
+net.add(Layer(25, 25, TAN_H, TAN_H_DERIVATIVE, Adam()))
+net.add(Layer(25, 25, TAN_H, TAN_H_DERIVATIVE, Adam()))
+net.add(Layer(25, 2, TAN_H, TAN_H_DERIVATIVE, Adam()))
+net.add(Layer(2, 25, TAN_H, TAN_H_DERIVATIVE, Adam()))
+net.add(Layer(25, 25, TAN_H, TAN_H_DERIVATIVE, Adam()))
+net.add(Layer(25, 25, TAN_H, TAN_H_DERIVATIVE, Adam()))
+net.add(Layer(25, 35, TAN_H, TAN_H_DERIVATIVE, Adam()))
 
 characters = get_characters()
 
@@ -39,13 +36,13 @@ noisy_characters = gaussian_noise(characters, MEAN, STD_DEVIATION)
 
 noisy_training_set = []
 for character in noisy_characters:
-    noisy_training_set.append([character])
+    noisy_training_set.append(character)
 
 training_expected = []
 for character in characters:
-    training_expected.append([character])
+    training_expected.append(character)
 
-noisy_training_set = np.array(noisy_training_set)
+noisy_training_set = np.array([noisy_training_set])
 
 test_set = np.array(gaussian_noise(characters.copy(), MEAN, STD_DEVIATION))
 
@@ -62,10 +59,10 @@ print(np.shape(test_expected))
 # Train nn
 net.use(mse, mse_prime)
 net.fit(training_set=noisy_training_set, test_set=test_set,
-        epochs=EPOCHS, learning_rate=LEARNING_RATE, training_expected=training_expected, test_expected=test_expected)
+        epochs=EPOCHS, learning_rate=LEARNING_RATE, training_expected=[training_expected], test_expected=test_expected)
 
 # Plot
-denoising_plot(net, characters)
+denoising_plot(net, characters, noisy_characters)
 
 ## PLUS: You can save the df it in a file
 # FileUtils.save_df_in_file("latent_space", pca_df)
--- tp5_/benchmarks/plot/denoising_plot.py
+++ tp5_/benchmarks/plot/denoising_plot.py
@@ -7,27 +7,27 @@ from classes.NeuralNetwork import NeuralNetwork
 from classes.NoiseFunctions import gaussian_noise
 
 MEAN = 0
-STD_DEVIATION = 0.1
+STD_DEVIATION = 0.09
 
 
-def denoising_plot(trained_nn: NeuralNetwork, characters):
+def denoising_plot(trained_nn: NeuralNetwork, characters, noisy_characters):
+
     # noisy image
-    data = [characters[1]]
-    data = trained_nn.predict(gaussian_noise(data, MEAN, STD_DEVIATION))
+    data = noisy_characters[1]
 
     arr = np.array(data)
     data = arr.reshape((7, 5))
 
-    fig = px.imshow(data)
+    fig = px.imshow(data, title="noisy", color_continuous_scale='gray')
     fig.show()
 
     # recovered data
-    data = trained_nn.predict(trained_nn.predict(characters[1]))
+    data = trained_nn.predict([noisy_characters])
 
-    arr = np.array(data)
+    arr = np.array(data[0][0][1].round().astype(int))
     data = arr.reshape((7, 5))
 
-    fig = px.imshow(data)
+    fig = px.imshow(data, title="recovered", color_continuous_scale='gray')
     fig.show()
 
     # original image
@@ -36,5 +36,5 @@ def denoising_plot(trained_nn: NeuralNetwork, characters):
     arr = np.array(data)
     data = arr.reshape((7, 5))
 
-    fig = px.imshow(data)
+    fig = px.imshow(data, title="original", color_continuous_scale='gray')
     fig.show()
