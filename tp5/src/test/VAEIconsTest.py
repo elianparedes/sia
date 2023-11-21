c1be0aced374ae7c09169121acae3d77224ee0ef
Martin Hexht <mhecht@itba.edu.ar>
2023-11-21 01:11:07 -0300

    Feat: VAE with

---
+++ tp5/src/test/VAEIconsTest.py
@@ -0,0 +1,66 @@
+from matplotlib import pyplot as plt
+import os
+import numpy as np
+from src.classes.functions.ActivationFunctions import TAN_H, TAN_H_DERIVATIVE
+from src.classes.models.NeuralNetwork import NeuralNetwork
+from src.classes.models.VariationalAutoencoder import VariationalAutoencoder
+from src.classes.layers.BiasedDenseLayer import BiasedDenseLayer
+from src.classes.optimizers.Adam import Adam
+from src.data.fonts import get_characters
+
+from src.utils.IconsUtils import IconsUtils
+
+FILE_PATH = os.path.join(os.pardir, "data", "small_icons.csv")
+
+# network
+encoder = NeuralNetwork(
+    activation=TAN_H,
+    activation_prime=TAN_H_DERIVATIVE,
+    optimizer=Adam,
+    biased=True,
+    architecture=[400, 150, 20, 4],
+    learning_rate=0.01
+)
+decoder = NeuralNetwork(
+    activation=TAN_H,
+    activation_prime=TAN_H_DERIVATIVE,
+    optimizer=Adam,
+    biased=True,
+    architecture=[2, 20, 150, 400],
+    learning_rate=0.01
+)
+
+vae = VariationalAutoencoder(encoder=encoder, decoder=decoder)
+characters = IconsUtils.load_icons_map_from_file(FILE_PATH)
+characters = np.array(list(characters))
+test_set = np.array(characters.copy())
+
+vae.train(test_set, epochs=8000)
+
+
+def plot_latent(vae, n=25, fig_size=15, digit_size=20):
+    figure = np.zeros((digit_size * n, digit_size * n))
+    grid_x = np.linspace(-1, 1, n)
+    grid_y = np.linspace(-1, 1, n)[::-1]
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z = np.array([[xi, yi]])
+            output = vae.predict(z)
+            digit = output[0].reshape(digit_size, digit_size)
+            figure[
+                i * digit_size: (i + 1) * digit_size,
+                j * digit_size: (j + 1) * digit_size,
+            ] = digit
+    plt.figure(figsize=(fig_size, fig_size))
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.imshow(figure, cmap="Greys_r")
+    plt.show()
+
+
+plot_latent(vae)
---
+++ tp5/src/utils/IconsUtils.py
@@ -0,0 +1,59 @@
+import os
+import ast
+from string import ascii_lowercase as alc
+from PIL import Image
+import numpy as np
+
+ICONS_PATH = os.path.join(os.pardir, "data", "small_icons")
+ICONS_FILE = os.path.join("../data/small_icons.csv")
+
+class IconsUtils:
+
+
+    @staticmethod
+    def load_icons_map_from_file(file_path):
+         # ICONS_NAMES = ["thumb_up", "home", "search", "share", "arrow", "person", "delete", "close", "thumb_down", "arrow_right", "arrow_back", "inv_close", "eva_arrow_right", "eva_delete", "eva_home", "eva_person", "eva_share", "lucide_arrow", "lucide_delete", "lucide_home", "lucide_user"]
+
+        ICONS_NAMES = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
+
+
+        icons = []
+
+        with open(file_path, 'r') as f:
+            for line in f:
+                line = line.strip()
+                matrix = ast.literal_eval(line)  # Convert string representation of list to actual list
+                icons.append(matrix)
+
+        alphabet = []
+        for i in alc:
+            alphabet.append(i)
+
+        # icons_map = {}
+        # for i, icon in enumerate(icons):
+        #     icons_map[ICONS_NAMES[i]] = icon
+
+        return icons
+
+    @staticmethod
+    def get_icon_matrix(icon):
+
+        img = Image.open(os.path.join(ICONS_PATH, icon)).convert('RGBA')
+        background = Image.new('RGBA', img.size, (255, 255, 255))
+        img = Image.alpha_composite(background, img).convert('L')
+
+        binary_matrix = np.array(img)
+        binary_matrix = np.where(binary_matrix > 245, -1, 1)
+
+        return binary_matrix
+
+# ICONS_PATH = os.path.join("../data/small_icons")
+
+# with open(ICONS_FILE, 'a') as file:
+#     for filename in os.listdir(ICONS_PATH):
+#         # file_path = os.path.join(ICONS_PATH, filename)
+#         matrix = IconsUtils.get_icon_matrix(filename)
+#         print(f"{matrix}\n")
+#         flattened_matrix = matrix.flatten()
+#         file.write("[" + ", ".join(map(str, flattened_matrix)) + "]\n")
+#
